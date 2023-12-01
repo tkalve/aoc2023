b273f688b7a0c016177a509a0f86daa981d62393
Thomas Kalve <thomas.kalve@bouvet.no>
2023-12-01 13:09:24 +0100

    Add lrt+trl match example and comments

--- 1/Program.cs
+++ 1/Program.cs
@@ -17,6 +17,14 @@ string? FromWord(string? input)
 
 foreach (var line in File.ReadLines(args[0]))
 {
+    /*  Example matching left to right and right to left:
+
+        var pattern = @"(" + string.Join('|', numbers) + @"|\d)";
+        var first = FromWord(Regex.Match(line, pattern).Value);
+        var last = FromWord(Regex.Match(line, pattern, RegexOptions.RightToLeft).Value);
+    */
+
+    // Look a positive lookahead 
     var matches = Regex.Matches(line, @"(?=(" + string.Join('|', numbers) + @"|\d))")
                         .Cast<Match>()
                         .Where(m => m.Groups[1].Success)
@@ -25,6 +33,7 @@ foreach (var line in File.ReadLines(args[0]))
     var first = FromWord(matches.FirstOrDefault()?.Groups[1].Value);
     var last = FromWord(matches.LastOrDefault()?.Groups[1].Value);
 
+    // Add first and last number combined (2 + four = 24) to total
     if (first != null && last != null)
         total += int.Parse($"{first}{last}");
 }
